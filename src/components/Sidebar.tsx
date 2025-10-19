598a48ed6f261a5df33550d4adcde6da71c6c070
sho-kudo <kudo.bizz@gmail.com>
2025-10-20 01:31:53 +0900

    fix: ダッシュボードのUI崩れを修正

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -71,7 +71,8 @@ const Sidebar = ({ className }: SidebarProps) => {
       {/* サイドバー */}
       <div
         className={cn(
-          'fixed inset-y-0 left-0 z-50 w-64 bg-white border-r border-gray-200 transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-0',
+          'fixed inset-y-0 left-0 z-50 w-64 bg-white border-r border-gray-200 transition-transform duration-300 ease-in-out',
+          'lg:translate-x-0',
           sidebarOpen ? 'translate-x-0' : '-translate-x-full',
           className
         )}
